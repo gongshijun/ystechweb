169a3413b67c7947bf296a832de5192db65c3da9
gongshijun <gongshijun@ict.ac.cn>
2017-02-17 13:59:50 +0800

    matlab

---
+++ handlers/matlab.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+import tornado.web
+
+class MMEHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render("matlab.html")
+
+    def post(self):
+        MME = self.get_argument("MME")
+        file_object = open('mme.m', 'w')
+        file_object.write(MME)
+        file_object.close()
+        self.write("sucess!")
