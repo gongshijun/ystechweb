16ea752dd860bab91ee5ff6447e77317ca204edd
gongshijun <2276195537@qq.com>
2017-02-17 21:43:55 +0800

    Update autocon.py

--- autoconict/autocon.py
+++ autoconict/autocon.py
@@ -10,12 +10,14 @@ values = {'uname': '', 'pass': ''}
 user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows 10)'
 headers = {'User-Agent': user_agent}
 data = urllib.parse.urlencode(values)
-data = data.decode("aciss")
+data = data.encode("aciss")
 request = urllib.request.Request(url, data)
 response = urllib.request.urlopen(request)
-con = response.read()
+con = response.read().decode("utf-8")
 p = '\d*\D'
 t = re.match(p, con)
 if t == None:
     print("success")
+else:
+    print("failed:"+con)
 pass
